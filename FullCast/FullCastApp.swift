a8dd743e827a6977fc6c176ef9394b7dcd7157b4
VishwaiOSDev <vishwa300499@icloud.com>
2022-02-03 13:35:00 +0530

    Inital Commit

---
+++ FullCast/FullCastApp.swift
@@ -0,0 +1,18 @@
+//
+//  FullCastApp.swift
+//  FullCast
+//
+//  Created by Vishwa  R on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FullCastApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FullCast/View/Authentication/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FullCast
+//
+//  Created by Vishwa  R on 03/02/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
